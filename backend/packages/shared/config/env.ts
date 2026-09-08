eb4264be191adcdcb3096ee8cc65f478b84676a8
AbbathiniRevanthKumar <revanani1801@gmail.com>
2026-09-09 00:20:58 +0530

    updated redis config - added password

--- backend/packages/shared/config/env.ts
+++ backend/packages/shared/config/env.ts
@@ -12,4 +12,5 @@ export const envConstants = {
   APP_ENV: appEnv,
   REDIS_HOST: process.env.REDIS_HOST || "localhost",
   REDIS_PORT: process.env.REDIS_PORT || 6379,
+  REDIS_PASSWORD : process.env.REDIS_PASSWORD
 };
--- backend/packages/shared/queue/connection.ts
+++ backend/packages/shared/queue/connection.ts
@@ -3,4 +3,5 @@ import { envConstants } from "../config/env";
 export const redisConnection = {
     host : envConstants.REDIS_HOST,
     port : Number(envConstants.REDIS_PORT),
+    password : envConstants.REDIS_PASSWORD
 }
